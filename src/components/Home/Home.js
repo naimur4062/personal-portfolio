4592e374578efb2f88d39dbd9010a70e47cc850f
naimur4062 <naimur4062@gmail.com>
2021-05-03 23:05:44 +0800

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../Header/Header';
 import SideBar from '../SideBar/SideBar';
-import About from '../About/About';
 
 const Home = () => {
     return (
